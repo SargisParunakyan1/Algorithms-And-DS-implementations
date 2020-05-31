93959a419563b9459a34bf1a0997442481a227b1
Sargis <parunakyano@gmail.com>
2020-05-31 11:13:57 +0400

    Remvoing unecessary asembly reference cahhce files from repo and adding Queue_ArrayList implementation

---
+++ Queueu/Queue_List.cs
@@ -0,0 +1,78 @@
+﻿
+namespace Queue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Queue_List<T> : IEnumerable<T>
+    {
+        #region Fields
+
+        private List<T> _items = new List<T>();
+
+        #endregion
+
+        #region Properties
+
+        public int Count
+        {
+            get
+            {
+                return _items.Count;
+            }
+        }
+
+        #endregion
+
+        #region Operations
+
+        public void Enqueue(T item)
+        {
+            _items.Add(item);
+        }
+
+        public T Dequeue()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+
+            T item = _items.FirstOrDefault();
+            _items.RemoveAt(0);
+
+            return item;
+        }
+
+        public T  Peek()
+        {
+            if (_items.Count == 0)
+            {
+                throw new InvalidOperationException("The queue is empty.");
+            }
+
+            T item = _items.FirstOrDefault();
+
+            return item;
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return _items.GetEnumerator();
+        }
+
+        #endregion
+    }
+}
